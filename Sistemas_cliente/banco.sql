5ebe20a8541d73715254b1371d8800633d3be71c
Joao18542 <joaobatiistaa12@gmail.com>
2025-11-14 17:13:52 -0300

    Add files via upload

---
+++ Sistemas_cliente/banco.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS clientes_db;
+USE clientes_db;
+CREATE TABLE IF NOT EXISTS clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    telefone VARCHAR(20)
+);
